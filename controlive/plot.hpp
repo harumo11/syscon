29beb5fff9169baa128ce41622243320ec588322
harumo11 <s_harumo@hotmail.co.jp>
2022-08-13 17:49:28 +0900

    scatter::plot() of double version is added

--- controlive/plot.hpp
+++ controlive/plot.hpp
@@ -18,23 +18,23 @@ public:
 
 private:
     FILE* pipe;
-    bool pipe_state = false;
-    bool open_gnuplot();
-    void close_gnuplot();
+    bool pipe_status = false;
+    bool open();
+    void close();
 };
 
 gnuplot::gnuplot()
 {
-    if (this->open_gnuplot() == false) {
-        this->pipe_state = false;
+    if (this->open() == false) {
+        this->pipe_status = false;
         std::cout << "|||[ERROR] Can not open the pipe to gnuplot. Did you install gnuplot ?" << std::endl;
         throw "Can not open the gnuplot via pipe";
     } else {
-        this->pipe_state = true;
+        this->pipe_status = true;
     }
 }
 
-bool gnuplot::open_gnuplot()
+bool gnuplot::open()
 {
     errno = 0;
     this->pipe = popen("gnuplot", "w");
@@ -45,18 +45,20 @@ bool gnuplot::open_gnuplot()
     }
 }
 
-void gnuplot::close_gnuplot()
+void gnuplot::close()
 {
-    Require(this->pipe_state == true, "Can not close gnuplot because gnuplot was not opend.");
+    Require(this->pipe_status == true,
+        "Can not close gnuplot because gnuplot was not opend.");
 
+    this->write("quit");
     pclose(this->pipe);
-    this->pipe_state = false;
+    this->pipe_status = false;
 }
 
 void gnuplot::write(std::string command)
 {
     Require(command.size() > 0, "The command is empty. The empty command can not be sent.");
-    Require(this->pipe_state == true, "Gnuplot was not open via pipe");
+    Require(this->pipe_status == true, "Gnuplot was not open via pipe");
 
     if (command.back() != '\n') {
         command += "\n";
@@ -78,14 +80,16 @@ public:
     void plot(const double x, const double y, bool add = true);
     void plot(const std::vector<double>& x, const std::vector<double>& y, bool add = false);
     void set_xlabel(const std::string xlabel);
-    void set_ylabel(const std::string ylable);
-    void set_window_size(const unsigned int w, const unsigned int h);
-    void show(bool pause);
+    void set_ylabel(const std::string ylabel);
+    void set_window_size(const unsigned int w = 640, const unsigned int h = 480);
+    void show(bool pause = false);
 
 private:
     void plot_and_add();
     void plot_once();
     gnuplot gnuplot_;
+    std::vector<double> x_plottable_data_;
+    std::vector<double> y_plottable_data_;
 };
 
 scatter::scatter()
@@ -95,5 +99,56 @@ scatter::scatter()
 scatter::~scatter()
 {
 }
+
+void scatter::autoscale(bool on)
+{
+    if (on == true) {
+        this->gnuplot_.write("set autoscale");
+    } else {
+        this->gnuplot_.write("unset autoscale");
+    }
+}
+
+void scatter::grid(bool on)
+{
+    if (on == true) {
+        this->gnuplot_.write("set grid lw 1.1");
+    } else {
+        this->gnuplot_.write("unset grid");
+    }
+}
+
+void scatter::plot(const double x, const double y, bool add)
+{
+    if (add == false) {
+        this->x_plottable_data_.clear();
+        this->y_plottable_data_.clear();
+    }
+
+    this->x_plottable_data_.push_back(x);
+    this->y_plottable_data_.push_back(x);
+}
+
+void scatter::plot(const std::vector<double>& x, const std::vector<double>& y, bool add) { }
+
+void scatter::set_xlabel(const std::string xlabel)
+{
+    std::string plot_command = "\"" + xlabel + "\"";
+    this->gnuplot_.write("set xl " + plot_command);
+}
+
+void scatter::set_ylabel(const std::string ylabel)
+{
+    std::string plot_command = "\"" + ylabel + "\"";
+    this->gnuplot_.write("set yl " + plot_command);
+}
+
+void scatter::set_window_size(const unsigned int w, const unsigned int h)
+{
+    std::string plot_command = "set terminal qt size " + std::to_string(w) + ", " + std::to_string(h);
+    this->gnuplot_.write(plot_command);
+}
+
+void scatter::show(bool pause) { }
 
 } // namespace controlive
