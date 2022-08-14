58af1be49246f3ddebe25b11ab6e5ca14df53573
harumo11 <s_harumo@hotmail.co.jp>
2022-08-14 18:43:41 +0900

    add 3rd test. To describe how to keep to open plot window.

--- test/visual_test_scatter.cpp
+++ test/visual_test_scatter.cpp
@@ -40,6 +40,8 @@ int main(int argc, char* argv[])
     std::cout << "This test describes how to keep to open the plot window." << std::endl;
     cntl::scatter plot3;
     std::vector<double> x = { 0.0, 1.0, 2.0, 3.0, 4.0 };
+    plot3.plot(x, x);
+    plot3.show(true);
 
     return 0;
 }
