945bf20b3936f2576b0212581ce8b70a226c587c
harumo11 <s_harumo@hotmail.co.jp>
2023-01-07 18:51:34 +0900

    add document

--- syscon/math.hpp
+++ syscon/math.hpp
@@ -3,24 +3,33 @@
 #include <iostream>
 #include <numbers>
 
+namespace syscon {
 
-namespace syscon{
-
-double T2fc(auto const T){
+double T2fc(auto const T)
+{
     return 1.0 / (static_cast<double>(T) * 2 * std::numbers::pi);
 }
 
-double wc2T(auto const T){
+/**
+ * @brief convert from cut-off frequency wc [rad/s] to time constant T [s].
+ *
+ * @param T Time domain [s]
+ *
+ * @return cut-off frequency [rad/s]
+ */
+double wc2T(auto const T)
+{
     return 1.0 / static_cast<double>(T);
 }
 
-double fc2wc(auto const fc){
+double fc2wc(auto const fc)
+{
     return 2 * std::numbers::pi * static_cast<double>(fc);
 }
 
-double fc2T(auto const fc){
+double fc2T(auto const fc)
+{
     return 1.0 / (2 * std::numbers::pi * static_cast<double>(fc));
 }
 
 }
-
